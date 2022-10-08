7edb69e5a981ec40eb7745500b081d424787d1a5
LiYang <foryli@163.com>
2022-10-08 15:41:50 +0800

    wip：add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+# Exit immediately if a simple command exits with a non-zero status.
+set -e
+
+# Build the package.
+yarn docs:build
+
+# Operations on directories and files.
+mkdir -p docs/.vuepress/.deploy
+cd docs/.vuepress/.deploy
+rm -rf *
+mkdir docs
+mv -f ../dist/* docs
+
+# Operations on git.
+git init
+git add -A
+git commit -m 'deploy'
+git branch -M main
+git push -f git@github.com:cicadasinging/cicadasinging.github.io.git main
